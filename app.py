28c747dfc77d9663aef2fa8d86c98938fbce0cb2
Linbin <i@teenet.me>
2022-06-02 23:52:07 -0400

    init

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+from flask_cors import CORS, cross_origin
+
+from matplotlib import pyplot as plt
+
+app = Flask(__name__, static_folder='client/build', static_url_path='')
+cors = CORS(app)
+
+
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
